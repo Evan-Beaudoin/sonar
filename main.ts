97b265d89e127e3849a284fedace55b3520ecde8
Evan-Beaudoin <71400127+Evan-Beaudoin@users.noreply.github.com>
2020-10-02 12:50:01 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
+// Created By: Evan
+// 
+// Created on: Oct. 2020
+// 
+// This program: Uses sonar
+let distance = 0
+basic.showIcon(IconNames.Yes)
 basic.forever(function () {
-	
+    distance = sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    )
+    basic.showNumber(distance)
+    basic.pause(200)
 })
